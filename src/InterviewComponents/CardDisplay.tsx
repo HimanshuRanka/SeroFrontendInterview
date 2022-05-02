97476b43ca925005031cc1d5e22f55e862b3e76b
HimanshuRanka <himanshuranka2612@gmail.com>
2022-05-02 16:09:34 -0400

    added in the coding questions and instructions

---
+++ src/InterviewComponents/CardDisplay.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { Card } from 'semantic-ui-react'
+
+//The Data for the first task
+import {CardDataTask1} from "./constants";
+
+//The Data for the third task
+import {CardDataTask3} from "./constants";
+
+/** Setup
+ * Open your terminal and run "npm start" so that we can see the results of our work.
+ */
+
+
+/**
+
+    Task 1: Use the Card prop along with any other JS method or React component to
+            display the data in CardDataTask1 in the <div> below in a single row.
+            Make sure all the cards are seperated by section and they dont flow off the page
+
+    Task 2: Add a property to each Card element such that the card displays a click-counter.
+            Set this counter to 1 when the page loads. Create a button that updates this
+            counter on every card when clicked. The update should take the previous value and double it
+    Ex:     on load: counter: 1
+            on first click: 2
+            on second click: 4
+            on third click: 8 ...
+
+    Task 3: The Data in CardDataTask3 has a field called "Countries." This is an array of strings.
+            Write a utility method that takes this array and converts it into a single comma
+            seperated string.
+            Oral: if you had to replace the first letter of each country with a number, how would u do it.
+    Ex:     [India, China, USA] --> "India, China, USA"
+
+ */
+
+
+export default function CardDisplay () {
+    return (
+        <div>
+            Your Code Goes Here
+        </div>
+    )
+}
---
+++ src/InterviewComponents/constants.ts
@@ -0,0 +1,85 @@
+export const CardDataTask1 = [
+    {
+        SectionHeading: "Section 1",
+        CardItems: [
+            {
+                Title: "Title1",
+                Description: "Some Values",
+                Date: "Today"
+            },
+            {
+                Title: "Title1",
+                Description: "Some Values",
+                Date: "Today"
+            },
+            {
+                Title: "Title1",
+                Description: "Some Values",
+                Date: "Today"
+            },
+        ]
+    },
+    {
+        SectionHeading: "Section 2",
+        CardItems: [
+            {
+                Title: "Title2",
+                Description: "Some Values",
+                Date: "Yesterday"
+            },
+            {
+                Title: "Title2",
+                Description: "Some Values",
+                Date: "Yesterday"
+            },
+            {
+                Title: "Title2",
+                Description: "Some Values",
+                Date: "Yesterday"
+            },
+        ]
+    }
+]
+
+export const CardDataTask3 = [
+    {
+        SectionHeading: "Section 1",
+        CardItems: [
+            {
+                Title: "Title1",
+                Description: "Some Values",
+                Countries: ["India", "China", "Singapore"]
+            },
+            {
+                Title: "Title1",
+                Description: "Some Values",
+                Countries: ["USA", "Canada", "Mexico"]
+            },
+            {
+                Title: "Title1",
+                Description: "Some Values",
+                Countries: ["England", "France", "Germany"]
+            },
+        ]
+    },
+    {
+        SectionHeading: "Section 2",
+        CardItems: [
+            {
+                Title: "Title2",
+                Description: "Some Values",
+                Countries: ["Peru", "Chile", "Brazil"]
+            },
+            {
+                Title: "Title2",
+                Description: "Some Values",
+                Countries: ["Australia", "New Zealand", "The Phillipines"]
+            },
+            {
+                Title: "Title2",
+                Description: "Some Values",
+                Countries: ["Congo", "Kenya", "Ethiopia"]
+            },
+        ]
+    }
+]
